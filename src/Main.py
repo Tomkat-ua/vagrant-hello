6f3bd2e2d009824bbcd2f25281fa95bc99aeaa43
Tomkat-ua <tomkat.s@gmail.com>
2021-10-05 22:00:48 +0000

    first commit

---
+++ src/Main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# -*- coding: UTF-8 -*-
+
+
+#################################
+# Vagrant Sandbox		#
+#################################
+import time
+
+while 1==1:
+  print ("US Robotics is loading ....")
+  time.sleep(2)
